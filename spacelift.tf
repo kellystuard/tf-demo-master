206cd1c2f96f53c2b8c786c2445ed065b4a2f636
Kelly Stuard <kellystuard@gmail.com>
2024-03-25 18:58:58 +0000

    Adding namespace for GitHub itnegration

--- spacelift.tf
+++ spacelift.tf
@@ -1,8 +1,13 @@
 resource "spacelift_stack" "zone" {
+  name = "${var.resource_prefix}zone"
+
   administrative = true
   autodeploy     = true
   branch         = "master"
   description    = "Zone Terraform repository for continuing the demo. Creates tf-demo-hub-* and tf-demo-spoke-*."
-  name           = "${var.resource_prefix}zone"
   repository     = "${var.resource_prefix}zone"
+
+  github_enterprise {
+    namespace = "kellystuard"
+  }
 }
