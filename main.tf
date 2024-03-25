ec02ebdbf4083efe9ae528ee74f56663376af4cd
Kelly Stuard <kellystuard@gmail.com>
2024-03-24 21:03:43 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+##
+# This area is used to define the master and zone areas. Changes to these settings 
+# is likely to have far-reaching ramifications and not be taken lightly.
+##
+locals {
+
+  master = {
+    cost_center = "88149"
+    owner       = "kellystuard@gmail.com"
+  }
+  
+  zone = {
+    cost_center = "88149"
+    owner       = "kellystuard@gmail.com"
+  }
+}
