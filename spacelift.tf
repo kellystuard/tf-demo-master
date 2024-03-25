9714fed245f5b22ca5e41b3a33710d5430e3fa3d
Kelly Stuard <kellystuard@gmail.com>
2024-03-25 18:47:51 +0000

    Setting up spacelift zone

--- spacelift.tf
+++ spacelift.tf
@@ -1,9 +1,8 @@
 resource "spacelift_stack" "zone" {
-  autodeploy        = true
-  branch            = "master"
-  description       = "Zone Terraform repository for continuing the demo. Creates tf-demo-hub-* and tf-demo-spoke-*."
-  name              = "${var.resource_prefix}zone"
-  project_root      = "tf-demo"
-  repository        = "github-kellystuard"
-  terraform_version = "1.5.7"
+  administrative = true
+  autodeploy     = true
+  branch         = "master"
+  description    = "Zone Terraform repository for continuing the demo. Creates tf-demo-hub-* and tf-demo-spoke-*."
+  name           = "${var.resource_prefix}zone"
+  repository     = "${var.resource_prefix}zone"
 }
