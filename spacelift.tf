701e902542a990a4f1d54d12b830596720bc71ee
Kelly Stuard <kellystuard@gmail.com>
2024-03-25 19:02:45 +0000

    Setting correct branch name

--- spacelift.tf
+++ spacelift.tf
@@ -3,7 +3,7 @@ resource "spacelift_stack" "zone" {
 
   administrative = true
   autodeploy     = true
-  branch         = "master"
+  branch         = "main"
   description    = "Zone Terraform repository for continuing the demo. Creates tf-demo-hub-* and tf-demo-spoke-*."
   repository     = "${var.resource_prefix}zone"
 
